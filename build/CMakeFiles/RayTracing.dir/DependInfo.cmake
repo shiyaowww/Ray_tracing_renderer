c113c84a20370d82eeabe31a2018602aac7e1b0b
shiyaowww <50718017+shiyaowww@users.noreply.github.com>
2020-09-17 23:20:12 -0700

    Add files via upload

---
+++ build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/BVH.cpp" "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/build/CMakeFiles/RayTracing.dir/BVH.cpp.o"
+  "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/Renderer.cpp" "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/build/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/Scene.cpp" "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/build/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/Vector.cpp" "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/build/CMakeFiles/RayTracing.dir/Vector.cpp.o"
+  "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/main.cpp" "/Users/shiyaow/Documents/Summer2020/GAMES101/hw7/src/build/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
